9cd4f81a7f42be35d669a7ab8e699afb8ccde192
Fernanda-15 <fernandaqp15@gmail.com>
2023-10-31 15:18:38 -0600

    Inicio del proyecto

---
+++ src/components/login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <form >
+        <h2>Login</h2>
+        <div class="input-field">
+          <input type="text" v-model="username" placeholder="Enter Username">
+        </div>
+        <div class="input-field">
+          <input type="password" v-model="password" placeholder="Enter Password">
+        </div>
+        <input type="submit" value="LogIn" v-on:click="submitForm()">
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      submitForm() {
+        fetch(this.url + '/.netlify/functions/user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password
+          })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.success) {
+            this.$router.push('/');
+          } else {
+            console.error(data.error);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      }
+    }
+  };
+  </script>
+  
